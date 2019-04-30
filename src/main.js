4badac18bdba16b145fa77b026ecd19a4fd9a016
Vladimir Abramyants <vabramanc@gmaill.com>
2019-05-01 00:50:50 +0300

    connect to firebase

--- src/main.js
+++ src/main.js
@@ -2,11 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebaseConfig from './config/firebase'
+import firebase from 'firebase'
+import 'firebase/firestore'
 
 Vue.config.productionTip = false
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+
+Vue.$db = db
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+
+  created() {
+    //load book
+    this.$store.dispatch('LOAD_BOOKS')
+  }
 }).$mount('#app')
