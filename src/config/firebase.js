f8209cfc8d84b153872192894fba56574872566a
Vladimir Abramyants <vabramanc@gmaill.com>
2019-05-01 00:55:50 +0300

    Created token for connect to firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyAnC61kPBeDl2dM8NyEYWlmvbukk3W8dPs",
+    authDomain: "tt-card-f3c6b.firebaseapp.com",
+    databaseURL: "https://tt-card-f3c6b.firebaseio.com",
+    projectId: "tt-card-f3c6b",
+    storageBucket: "tt-card-f3c6b.appspot.com",
+    messagingSenderId: "355856290038"
+  };
