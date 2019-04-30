4d9b08b4482e8a4fa3818426cd1936c699dba5e8
Vladimir Abramyants <vabramanc@gmaill.com>
2019-05-01 00:52:36 +0300

    created modules: cart and books and added in store

--- src/store.js
+++ src/store.js
@@ -3,14 +3,12 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+import cart from './store/cart.js'
+import books from './store/books.js'
+
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
+  modules: {
+    cart,
+    books
   }
 })
