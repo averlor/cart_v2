a37af8cc4ff60390979b1a7aa062f05f978844f7
Vladimir Abramyants <vabramanc@gmaill.com>
2019-05-01 00:53:13 +0300

    Edit home-page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,69 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <header>
+      <ShowCart @openCloseCart="openCloseCart"></ShowCart>
+      <cart v-show="drawer" @openCloseCart="openCloseCart"></cart>
+    </header>
+    <section class="wrapper__content">
+      <section class="filter"><Filters/></section>
+      <section class="product"><ProductList @openCloseCart="openCloseCart"/></section>
+    </section>
+    <footer>
+      <Info/>
+    </footer>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ProductList from '@/components/ProductList.vue'
+import cart from '@/components/Cart.vue'
+import ShowCart from '@/components/ButtonShowCart.vue'
+import Filters from '@/components/Filters.vue'
+import Info from '@/components/Info.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    ProductList,
+    cart,
+    ShowCart,
+    Filters,
+    Info
+  },
+  data() {
+    return {
+      drawer: false
+    }
+  },
+  methods: {
+    openCloseCart() {
+      this.drawer = !this.drawer
+    }
   }
+
 }
 </script>
+
+<style scoped>
+.wrapper{
+  width: 95vw;
+}
+header{
+  height: 3vh;
+  margin: 0 auto 1vh auto;
+}
+.wrapper__content{
+  height: 85vh;
+  margin: 3vh auto auto 0;
+}
+.product{
+  margin: 2vh auto;
+}
+footer{
+  height: 1vh;
+  margin: 2vh auto;
+  border-top: 1px solid gray;
+}
+</style>
+
