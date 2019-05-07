a038ca51e6aaf52ab1fcb757de3996c9d1213a5d
Vladimir Abramyants <vabramanc@gmaill.com>
2019-05-07 16:21:23 +0300

    content for each books brought out in new component

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="container">
       <div class="books" v-for="book in books" :key="book.id">
-        <div class="book">
-          <img :src="book.imageUrl" alt="Тут картинка" class="book__image">
-          <p class="book__genre">{{ book.genre[0] }}</p>
-          <p class="book__title">{{ book.title }}</p>
-          <p class="book__cost">Цена: {{ book.cost }} &#8381;</p>
-          <button @click.prevent="addToCart(book)" class="book__button button__buy">BUY</button>
-        </div>
+        <product :book="book" @addToCart="addToCart(book)"/>
       </div>
   </div>
 </template>
 
 <script>
+
+import Product from './Product.vue'
+
 export default {
   name: 'product-list',
+  components: {
+    Product
+  },
   computed: {
     books() {
       return this.$store.getters.GET_BOOKS
@@ -47,63 +47,6 @@ export default {
   grid-template-columns: repeat(3, minmax(160px, 1fr));
   gap: .5vw;
 }
-.book{
-  width: 30vw;
-  margin: 1vh auto;
-  display: grid;
-  align-content: center;
-  transition: all .4s ease;
-}
-.book__image{
-  margin: 0 auto 1vh auto;
-  padding-top: 1vh;
-}
-.book__genre{
-  width: 90%;
-  margin: 0 auto 1vh auto;
-  text-align: center;
-  font: 200 1em Arial;
-}
-.book__title{
-  width: 90%;
-  margin: 0 auto 1vh auto;
-  text-align: center;
-  font: 400 1.2em Arial;
-}
-.book__cost{
-  width: 90%;
-  margin: 0 auto 1vh auto;
-  text-align: center;
-  text-transform: uppercase;
-  font: 400 1.1em Arial;
-}
-.book__button{
-  width: 90%;
-  margin: 1vh auto;
-  text-align: center;
-}
-.button__buy{
-  border-radius: 5px;
-  width: 30%;
-  height: 4vh;
-  outline: none;
-  border: 1px solid lightgray;
-  background-color: transparent;
-  transition: background-color .3s ease;
-}
-.book:hover{
-  border: 1px solid #ccc;
-  z-index: 1;
-  border-radius: 5px;
-  box-shadow: 2px 3px 5px rgba(0,0,0,.5);
-  font-weight: 600;
-}
-.button__buy:hover{
-  background-color: rgb(24, 230, 58);
-  color: rgb(3, 3, 3);
-  border-color: rgb(3, 77, 15);
-}
-
 /* for medium screen */
 @media all and (max-width: 800px) and (min-width: 501px){
     .container{
